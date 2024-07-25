f84830c2a7080ff7b191183b9f6630dbfdc1abbc
German Velardez <germanvelardez16@gmail.com>
2024-07-25 15:24:00 -0300

    [test/test_node.c] Add test_increase_size()

--- test/test_node.c
+++ test/test_node.c
@@ -3,23 +3,30 @@
 //#include "types.h"     // header file with no *.c file -- no compilation/linking
 #include "node.h"       // source file foo.c under test
 
+list_t* list = NULL;
 
 
-void setUp(void) {}    // every test file requires this function;
-                       // setUp() is called by the generated runner before each test case function
+char line1[] = "This is a first line";
+char line2[] = "This is a second line";
+char line3[] = "This is the last line";
 
-void tearDown(void) {} // every test file requires this function;
-                       // tearDown() is called by the generated runner after each test case function
+void setUp(void) {
+    list = create_list();
+}   
+
+void tearDown(void) {
+    // Free memory
+    free(list);
+} 
 
 // a test case function
-void test_Foo_Node(void)
+void test_increase_size(void)
 {
+    TEST_ASSERT_EQUAL(0x0,list->size);
+    append(list,line1,strlen(line1)+1);
+    TEST_ASSERT_EQUAL(0x1,list->size);
+
 
 }
 
-// another test case function
-void test_Foo2_Node(void)
-{
-  
-}
 
